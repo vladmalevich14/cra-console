953bc216c759279c60c54f1254001c884f43adcc
vladmalevich14 <vladmalevich1431@gmail.com>
2022-08-10 23:10:00 +0300

    Commit - 14

--- src/Post/Post.tsx
+++ src/Post/Post.tsx
@@ -4,6 +4,8 @@ export const Post = () => {
     return (
         <div>
             <h2>Post5345</h2>
+            <h2>Post5345</h2>
+            <h2>Post5345</h2>
         </div>
     )
 }
--- src/User/User.tsx
+++ src/User/User.tsx
@@ -4,6 +4,11 @@ export const User = () => {
     return (
         <div>
             <h2>User453</h2>
+            <h2>User453</h2>
+            <h2>User453</h2>
+            <h2>User453</h2>
+            <h2>User453</h2>
+            <h2>User453</h2>
         </div>
     )
 }
